ec3efdbd1a8bea3a86cc34e6f2a14a9f6643b678
xwx <2249719242@qq.com>
2025-03-05 15:06:37 +0800

    新增mock功能

---
+++ mock/mock/index.js
@@ -0,0 +1,15 @@
+// const test = require('./test')
+const question = require('./question')
+const user = require('./user')
+// const stat = require('./stat')
+// const answer = require('./answer')
+
+const mockList = [
+  ...question,
+  ...user
+  // ...stat,
+  // ...answer
+  // ...test,
+]
+
+module.exports = mockList
